9f79e568b7fdad277f583d07304a0f0065b1f172
Thejal <76029279+Thejalmanoj@users.noreply.github.com>
2023-11-03 13:54:22 +0530

    Update letter_counter.py

--- letter_counter.py
+++ letter_counter.py
@@ -1,6 +1,3 @@
-# This is a sample Python script.
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 print("Hello user, this is a python program for letter counter")
 s=input("Enter the string:")
 let=input("Enter the letter to be searched:")
